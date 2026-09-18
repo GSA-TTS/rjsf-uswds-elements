1338a3b41360baf01d2dc1844713bb0e9a452d3e
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2026-09-18 04:12:00 +0000

    feat: add uswds button element

--- packages/rjsf-uswds/src/custom-elements.d.ts
+++ packages/rjsf-uswds/src/custom-elements.d.ts
@@ -6,10 +6,15 @@ type UswdsAlertAttributes = HTMLAttributes<HTMLElement> & {
   type?: 'info' | 'warning' | 'error' | 'success';
 };
 
+type UswdsButtonAttributes = HTMLAttributes<HTMLElement> & {
+  variant?: 'primary' | 'outline' | 'unstyled';
+};
+
 declare module 'react/jsx-runtime' {
   namespace JSX {
     interface IntrinsicElements {
       'uswds-alert': DetailedHTMLProps<UswdsAlertAttributes, HTMLElement>;
+      'uswds-button': DetailedHTMLProps<UswdsButtonAttributes, HTMLElement>;
       'uswds-error-message': DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
       'uswds-required-marker': DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
     }
